fc9dd9dc3d0377858d7c6d467e68890878109199
tepete <pifroggimc@gmail.com>
2024-06-01 23:40:35 +0200

    Changed Butteraugli plugin to faster implementation (suggested by NSQY) and made script more robust to more input formats.

--- vs_align/temporal_alignment.py
+++ vs_align/temporal_alignment.py
@@ -8,30 +8,37 @@ def temporal(clip, ref, clip2=None, tr=30, fallback=None, thresh=40, precision=1
 
     #checks
     if clip.format.id != ref.format.id:
-        raise ValueError("clip and ref must be the same format.")
+        raise ValueError("Clip and ref must be the same format.")
     if clip.width != ref.width or clip.height != ref.height:
-        raise ValueError("clip and ref must have the same dimensions.")
+        raise ValueError("Clip and ref must have the same dimensions.")
     if clip2 is not None and fallback is not None:
         if clip2.format.id != fallback.format.id:
-            raise ValueError("clip2 and fallback must be the same format.")
+            raise ValueError("Clip2 and fallback must be the same format.")
         if clip2.width != fallback.width or clip2.height != fallback.height:
-            raise ValueError("clip2 and fallback must have the same dimensions.")
+            raise ValueError("Clip2 and fallback must have the same dimensions.")
     if clip2 is None and fallback is not None:
         if clip.format.id != fallback.format.id:
-            raise ValueError("clip and fallback must be the same format.")
+            raise ValueError("Clip and fallback must be the same format.")
         if clip.width != fallback.width or clip.height != fallback.height:
-            raise ValueError("clip and fallback must have the same dimensions.")
+            raise ValueError("Clip and fallback must have the same dimensions.")
+    if clip_num is not None and clip_den is not None and ref_num is not None and ref_den is not None:
+        if clip.format.color_family != vs.YUV:
+            raise ValueError("All input clips must be in YUV format when using clips with different frame rates.")
+
+    #if clip2 is not set, use clip
+    if clip2 is None:
+        clip2 = clip
 
     #precision modes
     if precision == 2:
-        process = core.butteraugli.butteraugli
-        prop_key = "_Diff"
+        process = core.julek.Butteraugli
+        prop_key = "_FrameButteraugli"
         process_name = "Butteraugli"
     else: #precision == 1
         process = core.std.PlaneStats
         prop_key = "PlaneStatsDiff"
         process_name = "PlaneStats"
-    
+
     if clip_num is not None and clip_den is not None and ref_num is not None and ref_den is not None:
         #double framerate to make sure no frames are lost during resampling
         ref = core.resize.Bicubic(ref, width=clip.width, height=clip.height)
@@ -46,17 +53,26 @@ def temporal(clip, ref, clip2=None, tr=30, fallback=None, thresh=40, precision=1
         clip = core.std.AssumeFPS(clip, fpsnum=clip_num*2, fpsden=clip_den)
         clip_tivtc = core.tivtc.TDecimate(clip, rate=ref_num/ref_den, mode=7)
         clip_tivtc = core.std.AssumeFPS(clip_tivtc, fpsnum=ref_num, fpsden=ref_den)
-        if clip2 is not None:
-            clip2 = core.std.Interleave([clip2, clip2])
-            clip2 = core.std.AssumeFPS(clip2, fpsnum=clip_num*2, fpsden=clip_den)
-            clip2 = core.tivtc.TDecimate(clip=clip, clip2=clip2, rate=ref_num/ref_den, mode=7)
-            clip2 = core.std.AssumeFPS(clip2, fpsnum=ref_num, fpsden=ref_den)
-        clip = clip_tivtc
         
-        #convert to RGB24 for butteraugli
+        clip2 = core.std.Interleave([clip2, clip2])
+        clip2 = core.std.AssumeFPS(clip2, fpsnum=clip_num*2, fpsden=clip_den)
+        clip2 = core.tivtc.TDecimate(clip=clip, clip2=clip2, rate=ref_num/ref_den, mode=7)
+        clip2 = core.std.AssumeFPS(clip2, fpsnum=ref_num, fpsden=ref_den)
+        clip = clip_tivtc
+
+    #convert to RGB24 for butteraugli based on the color family
+    if clip.format.color_family == vs.YUV:
         clip = core.resize.Point(clip, format=vs.RGB24, matrix_in_s="709", range_in_s='full', range_s='full')
         ref = core.resize.Point(ref, format=vs.RGB24, matrix_in_s="709", range_in_s='full', range_s='full')
-    
+    elif clip.format.color_family == vs.RGB:
+        clip = core.resize.Point(clip, format=vs.RGB24)
+        ref = core.resize.Point(ref, format=vs.RGB24)
+    elif clip.format.color_family == vs.GRAY:
+        clip = core.resize.Point(clip, format=vs.RGB24)
+        ref = core.resize.Point(ref, format=vs.RGB24)
+    else:
+        raise ValueError("Unsupported format.")
+
     #helper function to generate shifts
     def gen_shifts(c, n, forward=True, backward=True):
         shifts = [c]
@@ -70,11 +86,11 @@ def temporal(clip, ref, clip2=None, tr=30, fallback=None, thresh=40, precision=1
     #generate shifted clips
     if clip_num is not None and clip_den is not None and ref_num is not None and ref_den is not None:
         clip = gen_shifts(clip, tr*2)
-        clip2 = gen_shifts(clip2, tr*2) if clip2 else clip
+        clip2 = gen_shifts(clip2, tr*2)
     else:
         clip = gen_shifts(clip, tr)
-        clip2 = gen_shifts(clip2, tr) if clip2 else clip
-    
+        clip2 = gen_shifts(clip2, tr)
+
     diffs = [process(c, ref) for c in clip]
     indices = list(range(len(diffs)))
     do_debug, alignment = debug if isinstance(debug, tuple) else (debug, 7)
@@ -102,5 +118,4 @@ def temporal(clip, ref, clip2=None, tr=30, fallback=None, thresh=40, precision=1
     #half framerate to get original one
     if clip_num is not None and clip_den is not None and ref_num is not None and ref_den is not None:
         result = core.std.SelectEvery(clip=result, cycle=2, offsets=0)
-    
     return result
