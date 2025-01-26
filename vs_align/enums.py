92448fc5c63b7c7be361530d2354daad4523a880
pifroggi <pifroggimc@gmail.com>
2025-01-26 15:54:29 +0100

    update enums

--- vs_align/enums.py
+++ vs_align/enums.py
@@ -1,17 +1,16 @@
 from enum import Enum
 
 class Device(Enum):
-    CPU = "cpu"
+    CPU  = "cpu"
     CUDA = "cuda"
 
 class SpatialPrecision(Enum):
-    PERCENT_50 = 1
+    PERCENT_50  = 1
     PERCENT_100 = 2
     PERCENT_200 = 3
     PERCENT_400 = 4
-    PERCENT_800 = 5
 
 class TemporalPrecision(Enum):
-    PlaneStats = 1
-    Butteraugli = 2
-    TOPIQ = 3
+    PLANESTATS  = 1
+    BUTTERAUGLI = 2
+    TOPIQ       = 3
